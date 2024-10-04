af1fb38714b6b6b6866aa0ac26dbd87a412d79d3
Daly <monokakoman@gmail.com>
2024-10-04 12:52:48 +0300

    Refactor HomeViewBody Widget and Add ProductItem and CustomSliverGridView Components
    - Increased the height of the SizedBox widget from 20 to 30.
    - Added imports for `font_awesome_flutter`, `foodgo/core/utils/images`, and `foodgo/core/utils/styles`.
    - Introduced a new `ProductItem` widget to display product details with an image, title, subtitle, rating, and a heart icon.
    - Created a `CustomSliverGridView` widget to display a grid of `ProductItem` widgets with a fixed cross-axis count and spacing.
    - Integrated the `CustomSliverGridView` into the `HomeViewBody` widget, replacing the previous placeholder.

--- lib/featuers/home/presentation/views/widgets/home_view_body.dart
+++ lib/featuers/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:foodgo/core/utils/images.dart';
+import 'package:foodgo/core/utils/styles.dart';
 import 'package:foodgo/featuers/home/presentation/views/widgets/custom_category_list_view.dart';
 import 'package:foodgo/featuers/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:foodgo/featuers/home/presentation/views/widgets/custom_search_bar.dart';
@@ -34,11 +37,101 @@ class HomeViewBody extends StatelessWidget {
         ),
         SliverToBoxAdapter(
           child: SizedBox(
-            height: 20,
+            height: 30,
           ),
         ),
         SliverToBoxAdapter(child: CustomCategoryListView()),
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 30,
+          ),
+        ),
+        SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            sliver: CustomSliverGridView()),
       ],
     );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            blurRadius: 4,
+            spreadRadius: 4,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            Assets.imagesBurger5,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                'Chesseburger',
+                style: TextStyles.font20Medium(context).copyWith(
+                  color: const Color(0xff3C2F2F),
+                ),
+              ),
+              subtitle: Text(
+                'Wendy\'s Burger',
+                style: TextStyles.font18Regular(context).copyWith(
+                  color: const Color(0xff3C2F2F),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.star,
+                  color: Color(0xffFF9633),
+                ),
+                Text(
+                  '4.9',
+                  style: TextStyles.font18Medium(context)
+                      .copyWith(color: const Color(0xff3C2F2F)),
+                ),
+                const Spacer(),
+                const Icon(FontAwesomeIcons.heart),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSliverGridView extends StatelessWidget {
+  const CustomSliverGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 185 / 225,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        itemBuilder: (context, index) {
+          return const ProductItem();
+        });
   }
 }
