c8e55e24182261966f61c3e8fd4159cdfa3950f8
Daly <monokakoman@gmail.com>
2024-10-04 09:34:50 +0300

    Add Assets and Fonts for Flutter Project
    This commit introduces a comprehensive set of assets and fonts to enhance the visual and typographical aspects of the Flutter project. The following changes have been made:
    
    1. **Font Assets**:
       - Added various font files including `Lobster-Regular.ttf`, `Poppins-Medium.ttf`, and multiple weights of `Roboto` (Regular, Medium, Bold).
       - Configured these fonts in the `pubspec.yaml` file to ensure they are properly recognized and utilized within the application.
    
    2. **Image Assets**:
       - Added a variety of image files such as `avatar.png`, `backen.png`, and multiple burger images (`burger1.png` to `burger7.png`).
       - Included additional images like `crispy_onion.png`, `cucumber.png`, `frize.png`, and others to enrich the visual content of the app.
       - Updated the `pubspec.yaml` file to include the image assets directory, ensuring all images are bundled with the application.
    
    3. **Utility Files**:
       - Created `images.dart` in the `lib/core/utils/` directory to manage and reference image assets programmatically.
       - Added `styles.dart` in the same directory to define responsive text styles using various font sizes and weights, ensuring a consistent and scalable typography across different screen sizes.
    
    These additions and configurations are crucial for maintaining a visually appealing and responsive user interface, leveraging the power of custom fonts and high-quality images.

---
+++ lib/core/utils/images.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.png
+  static const String imagesAvatar = "assets/images/avatar.png";
+
+  /// Assets for imagesBacken
+  /// assets/images/backen.png
+  static const String imagesBacken = "assets/images/backen.png";
+
+  /// Assets for imagesBurger1
+  /// assets/images/burger1.png
+  static const String imagesBurger1 = "assets/images/burger1.png";
+
+  /// Assets for imagesBurger2
+  /// assets/images/burger2.png
+  static const String imagesBurger2 = "assets/images/burger2.png";
+
+  /// Assets for imagesBurger3
+  /// assets/images/burger3.png
+  static const String imagesBurger3 = "assets/images/burger3.png";
+
+  /// Assets for imagesBurger4
+  /// assets/images/burger4.png
+  static const String imagesBurger4 = "assets/images/burger4.png";
+
+  /// Assets for imagesBurger5
+  /// assets/images/burger5.png
+  static const String imagesBurger5 = "assets/images/burger5.png";
+
+  /// Assets for imagesBurger6
+  /// assets/images/burger6.png
+  static const String imagesBurger6 = "assets/images/burger6.png";
+
+  /// Assets for imagesBurger7
+  /// assets/images/burger7.png
+  static const String imagesBurger7 = "assets/images/burger7.png";
+
+  /// Assets for imagesCapotche
+  /// assets/images/capotche.png
+  static const String imagesCapotche = "assets/images/capotche.png";
+
+  /// Assets for imagesCrispyOnion
+  /// assets/images/crispy_onion.png
+  static const String imagesCrispyOnion = "assets/images/crispy_onion.png";
+
+  /// Assets for imagesCucumber
+  /// assets/images/cucumber.png
+  static const String imagesCucumber = "assets/images/cucumber.png";
+
+  /// Assets for imagesFrize
+  /// assets/images/frize.png
+  static const String imagesFrize = "assets/images/frize.png";
+
+  /// Assets for imagesMaster
+  /// assets/images/master.png
+  static const String imagesMaster = "assets/images/master.png";
+
+  /// Assets for imagesOnion
+  /// assets/images/onion.png
+  static const String imagesOnion = "assets/images/onion.png";
+
+  /// Assets for imagesSalad
+  /// assets/images/salad.png
+  static const String imagesSalad = "assets/images/salad.png";
+
+  /// Assets for imagesTomato
+  /// assets/images/tomato.png
+  static const String imagesTomato = "assets/images/tomato.png";
+
+  /// Assets for imagesVisa
+  /// assets/images/visa.png
+  static const String imagesVisa = "assets/images/visa.png";
+}
