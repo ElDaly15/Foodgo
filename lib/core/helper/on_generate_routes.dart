78e8b545a300e3add6ee13a131655894f932b9c5
Daly <monokakoman@gmail.com>
2024-10-04 10:02:05 +0300

    Add HomeView and update SplashView to navigate to HomeView
    This commit introduces the `HomeView` and its associated widget `HomeViewBody`. The `HomeView` is now integrated into the app's routing system, allowing it to be navigated to from the `SplashView` after a delay of 3 seconds. The `SplashView` has been updated to be a `StatefulWidget` to handle the navigation logic. Additionally, the `onGenerateRoute` function in the routing helper has been updated to include the `HomeView` route.

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:foodgo/featuers/home/presentation/views/home_view.dart';
 import 'package:foodgo/featuers/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (_) => const SplashView());
-
+    case HomeView.routeName:
+      return MaterialPageRoute(builder: (_) => const HomeView());
     default:
       return MaterialPageRoute(builder: (_) => const Scaffold());
   }
--- lib/featuers/splash/presentation/views/splash_view.dart
+++ lib/featuers/splash/presentation/views/splash_view.dart
@@ -1,10 +1,27 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:foodgo/featuers/home/presentation/views/home_view.dart';
 import 'package:foodgo/featuers/splash/presentation/views/widgets/splash_view_body.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   static const routeName = "splashView";
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed(HomeView.routeName);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
